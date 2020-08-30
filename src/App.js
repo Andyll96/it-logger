c0f686af5894fe251172441f60f5df7d2da1642c
Andy Llactahuamani - Desktop <andy.llactahuamani@ryerson.ca>
2020-08-30 09:10:35 -0400

    types.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, Fragment} from 'react';
+import React, { useEffect, Fragment } from 'react';
 import SearchBar from './components/layout/SearchBar'
 import Logs from './components/logs/Logs'
 import AddBtn from './components/layout/AddBtn'
@@ -6,29 +6,33 @@ import AddLogModal from './components/logs/AddLogModal'
 import EditLogModal from './components/logs/EditLogModal'
 import AddTechModal from './components/techs/AddTechModal'
 import TechListModal from './components/techs/TechListModal'
+import { Provider } from 'react-redux'
+import store from './store'
 
 import 'materialize-css/dist/css/materialize.min.css'
 import M from 'materialize-css/dist/js/materialize.min.js'
 import './App.css';
 
-const App= () => {
-  useEffect(()=>{
+const App = () => {
+  useEffect(() => {
     // Init Materialize JS
     M.AutoInit();
   });
 
   return (
-    <Fragment>
-      <SearchBar/>
-      <div className='container'>
-        <AddBtn/>
-        <AddLogModal/>
-        <EditLogModal/>
-        <AddTechModal/>
-        <TechListModal/>
-        <Logs/>
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   );
 }
 
