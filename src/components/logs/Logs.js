7bc08a6e09c65ab9f7d3e4b3ca2eb3f8405465ef
Andy Llactahuamani - Desktop <andy.llactahuamani@ryerson.ca>
2020-08-31 00:31:29 -0400

    connecting redux to a componet

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,26 +1,21 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
+import {connect} from 'react-redux';
 import LogItem from './LogItem'
 import Preloader from '../layout/Preloader'
+import PropTypes from 'prop-types'
+import {getLogs} from '../../actions/logActions';
 
-const Logs = () => {
-    const [logs, setLogs] = useState([]);
-    const [loading, setLoading] = useState(false);
+// logs and loading is being destructed from log
+// 
+const Logs = ({log: {logs, loading}, getLogs}) => {
+
 
     useEffect(() => {
         getLogs();
         // eslint-disable-next-line
     }, []);
 
-    const getLogs = async () => {
-        setLoading(true);
-        const res = await fetch('/logs');
-        const data = await res.json();
-
-        setLogs(data);
-        setLoading(false);
-    }
-
-    if (loading) {
+    if (loading || logs === null) {
         return <Preloader/>
     }
 
@@ -34,4 +29,15 @@ const Logs = () => {
     )
 }
 
-export default Logs
+Logs.propTypes = {
+    log: PropTypes.object.isRequired
+};
+
+// if you want to get anything from app level state to component you bring it in as a prop
+// log: state.log comes from index.js
+const mapStateToProps = state => ({
+    log: state.log
+});
+
+// when you bring in an action, and pass it in here, your action is now a prop so it's destructured at the top
+export default connect(mapStateToProps, {getLogs})(Logs);
--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,3 +1,6 @@
+import {GET_LOGS, SET_LOADING, LOGS_ERROR} from '../actions/types'
+
+
 const initialState = {
     logs: null,
     current: null,
@@ -5,12 +8,26 @@ const initialState = {
     error: null
 }
 
+// Get logs from server
 export default(state = initialState, action) => {
     switch (action.type) {
-        case value:
-            
-            break;
-    
+        case GET_LOGS:
+            return{
+                ...state,
+                logs: action.payload,
+                loading: false
+            }
+        case SET_LOADING:
+            return{
+                ...state,
+                loading: true
+            }
+        case LOGS_ERROR:
+            console.error(action.payload);
+            return{
+                ...state,
+                error: action.payload
+            }    
         default:
             return state;
     }
